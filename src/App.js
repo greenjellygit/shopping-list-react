9bbc957e40a425d93d087534797f767391fb4cae
kantczak <kantczak>
2019-02-27 09:44:26 +0100

    Prepare base project for new group

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
-import {SearchPage} from "./containers/search.page";
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <SearchPage/>
+            <div style={{textAlign: 'center', marginTop: '100px'}}>
+                <h1>Hello world!</h1>
             </div>
         );
     }
